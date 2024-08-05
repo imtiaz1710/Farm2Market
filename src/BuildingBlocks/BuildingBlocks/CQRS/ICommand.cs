71e72e7a3111ba00f08e588c7873d6765a17dbd4
imtiaz1710 <imtiaz.mehedi.iubat@gmail.com>
2024-08-05 06:46:11 +0600

    building block done

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+public interface ICommand: ICommand<Unit> //unit is the void type of mediatr
+{
+
+}
+
+public interface ICommand<out TResponse> : IRequest<TResponse>
+{
+}

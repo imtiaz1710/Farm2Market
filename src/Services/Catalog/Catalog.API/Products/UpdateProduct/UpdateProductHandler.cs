ad81e3a6e589d27e6754a26e41250c0935016ef1
imtiaz1710 <imtiaz.mehedi.iubat@gmail.com>
2024-08-10 13:43:25 +0600

    update product done

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<UpdateProductResult>;
+public record UpdateProductResult(bool IsUpdated);
+internal class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+{
+    public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"UpdateProductHandler.Handle called with {command}");
+
+        var product = await session.LoadAsync<Product>(command.Id);
+
+        if (product is null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        product = command.Adapt<Product>();
+
+        session.Update(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new UpdateProductResult(true);
+    }
+}

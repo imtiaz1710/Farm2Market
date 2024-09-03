1afcbce1ce35d8ce874fe0f7bc8cbbb1de503c97
imtiaz1710 <imtiaz.mehedi.iubat@gmail.com>
2024-09-03 09:57:51 +0600

    seeding and pagination done

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,17 +1,17 @@
-﻿using Catalog.API.Products.CreateProduct;
+﻿namespace Catalog.API.Products.GetProducts;
 
-namespace Catalog.API.Products.GetProducts;
-
-//public record GetProductsRequest();
+public record GetProductsRequest(int? PageNo = 1, int? PageSize = 10);
 public record GetProductsResponse(IEnumerable<Product> Products);
 
 public class GetProductsEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/products", async (ISender sender) =>
+        app.MapGet("/products", async ([AsParameters] GetProductsRequest request, ISender sender) =>
         {
-            var result = await sender.Send(new GetProductQuery());
+            var query = request.Adapt<GetProductQuery>();
+
+            var result = await sender.Send(query);
 
             var response = result.Adapt<GetProductsResponse>();
 
--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,13 +1,15 @@
 ﻿
+using Marten.Pagination;
+
 namespace Catalog.API.Products.GetProducts;
 
-public record GetProductQuery() : IQuery<GetProductResult>;
+public record GetProductQuery(int? PageNo = 1, int? PageSize = 10) : IQuery<GetProductResult>;
 public record GetProductResult(IEnumerable<Product> Products);
 internal class GetProductsQueryHandler(IDocumentSession session) : IQueryHandler<GetProductQuery, GetProductResult>
 {
     public async Task<GetProductResult> Handle(GetProductQuery query, CancellationToken cancellationToken)
     {
-        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+        var products = await session.Query<Product>().ToPagedListAsync(query.PageNo ?? 1, query.PageSize ?? 10, cancellationToken);
 
         return new GetProductResult(products);
     }

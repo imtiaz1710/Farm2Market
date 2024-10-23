d125951e157b180fda65c71a3dd1ed8f6332d879
imtiaz1710 <imtiaz.mehedi.iubat@gmail.com>
2024-10-23 22:40:03 +0600

    dependency file added

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,3 +1,12 @@
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApplicationServices();
+
 var app = builder.Build();
 app.Run();

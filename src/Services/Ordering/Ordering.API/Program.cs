624409df90823e976a48fee4c2ed7b9e690085eb
imtiaz1710 <imtiaz.mehedi.iubat@gmail.com>
2024-12-05 21:42:30 +0600

    auto migrtating by extension in startup done

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,12 +1,23 @@
+using Ordering.API;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddApplicationServices()
     .AddInfrastructureServices(builder.Configuration)
-    .AddApplicationServices();
+    .AddApplicationServices()
+    .AddAPIServices();
 
 var app = builder.Build();
+
+app.UseAPIServices();
+
+if (app.Environment.IsDevelopment())
+{
+    await app.InitializeDatabaseAsync();
+}
+
 app.Run();

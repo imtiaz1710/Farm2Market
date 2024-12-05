2a5c036024a0a1d738c0d7df59fdf6626da339bc
imtiaz1710 <imtiaz.mehedi.iubat@gmail.com>
2024-12-05 22:46:59 +0600

    initial data done

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/InitialData.cs
@@ -0,0 +1,57 @@
+﻿using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Infrastructure.Data.Extensions;
+
+internal class InitialData
+{
+    public static IEnumerable<Customer> Customers =>
+  new List<Customer>
+  {
+        Customer.Create(CustomerId.Of(new Guid("58c49479-ec65-4de2-86e7-033c546291aa")), "Imtiaz", "imtiaz.mehedi.iubat@gmail.com"),
+        Customer.Create(CustomerId.Of(new Guid("189dc8dc-990f-48e0-a37b-e6f2b60b9d7d")), "Mehedi", "imtiaz.mehedi.iubat2@gmail.com")
+  };
+
+    public static IEnumerable<Product> Products =>
+        new List<Product>
+        {
+            Product.Create(ProductId.Of(new Guid("5334c996-8457-4cf0-815c-ed2b77c4ff61")), "Rice", 500),
+            Product.Create(ProductId.Of(new Guid("c67d6323-e8b1-4bdf-9a75-b0d0d2e7e914")), "Suger", 400),
+            Product.Create(ProductId.Of(new Guid("4f136e9f-ff8c-4c1f-9a33-d12f689bdab8")), "Mango", 650),
+            Product.Create(ProductId.Of(new Guid("6ec1297b-ec0a-4aa1-be25-6726e3b51a27")), "Banana", 450)
+        };
+
+    public static IEnumerable<Order> OrdersWithItems
+    {
+        get
+        {
+            var address1 = Address.Of("Imtiaz", "Zaman", "imtiaz.mehedi.iubat@gmail.com","Tongi-Gazipur", "Bangladesh", "Dhaka", "1200");
+            var address2 = Address.Of("Mehedi", "Zaman", "imtiaz.mehedi.iubat2@gmail.com", "Tongi-Gazipur", "Bangladesh", "Dhaka", "1200");
+
+            var payment1 = Payment.Of("Imtiaz", "5555555555554444", "12/28", "355", 1);
+            var payment2 = Payment.Of("Mehedi", "8885555555554444", "06/30", "222", 2);
+
+            var order1 = Order.Create(
+                            OrderId.Of(Guid.NewGuid()),
+                            CustomerId.Of(new Guid("58c49479-ec65-4de2-86e7-033c546291aa")),
+                            OrderName.Of("ORD_1"),
+                            shippingAddress: address1,
+                            billingAddress: address1,
+                            payment1);
+            order1.Add(ProductId.Of(new Guid("5334c996-8457-4cf0-815c-ed2b77c4ff61")), 2, 500);
+            order1.Add(ProductId.Of(new Guid("c67d6323-e8b1-4bdf-9a75-b0d0d2e7e914")), 1, 400);
+
+            var order2 = Order.Create(
+                            OrderId.Of(Guid.NewGuid()),
+                            CustomerId.Of(new Guid("189dc8dc-990f-48e0-a37b-e6f2b60b9d7d")),
+                            OrderName.Of("ORD_2"),
+                            shippingAddress: address2,
+                            billingAddress: address2,
+                            payment2);
+            order2.Add(ProductId.Of(new Guid("4f136e9f-ff8c-4c1f-9a33-d12f689bdab8")), 1, 650);
+            order2.Add(ProductId.Of(new Guid("6ec1297b-ec0a-4aa1-be25-6726e3b51a27")), 2, 450);
+
+            return new List<Order> { order1, order2 };
+        }
+    }
+}

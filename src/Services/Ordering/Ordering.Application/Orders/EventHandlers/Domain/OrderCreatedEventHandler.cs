b7abd267a47172092b91422fa0db40ef9532aafe
imtiaz1710 <imtiaz.mehedi.iubat@gmail.com>
2024-12-14 22:11:29 +0600

    basket checkout event handler done

--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,14 +1,20 @@
-﻿using MediatR;
+﻿using MassTransit;
+using MediatR;
 using Microsoft.Extensions.Logging;
+using Ordering.Application.Extensions;
 using Ordering.Domain.Events;
 
 namespace Ordering.Application.Orders.EventHandlers.Domain;
 public class OrderCreatedEventHandler
-    (ILogger<OrderCreatedEventHandler> logger)
+    (ILogger<OrderCreatedEventHandler> logger, IPublishEndpoint publishEndpoint)
     : INotificationHandler<OrderCreatedEvent>
 {
     public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
     {
+        var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
+
+        await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+
         logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
     }
 }
